e3068fdbdaf46e2fca8039d361e67a191d12c91d
OuOu2021 <1113117424@qq.com>
2024-09-29 21:17:34 +0800

    update

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,30 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.is_empty() {
+            return Self::default();
+        }
+
+        let (name, age) = match s.split_once(',') {
+            Some(pair) => pair,
+            None => return Self::default(),
+        };
+
+        let age = match age.parse::<usize>() {
+            Ok(age) => age,
+            Err(_) => return Self::default(),
+        };
+
+        if name.is_empty() {
+            return Self::default();
+        }
+
+        Person {
+            name: name.to_owned(),
+            age,
+        }
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,8 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -52,6 +50,34 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.is_empty() {
+            return Err(ParsePersonError::Empty);
+        }
+
+        let mut splits = s.split(',').collect::<Vec<&str>>();
+        if splits.len() != 2 {
+            return Err(ParsePersonError::BadLen);
+        }
+        let name = splits[0];
+
+        let mut age = 0;
+        match splits[1].parse::<usize>() {
+            Ok(a) => {
+                age = a;
+            }
+            Err(p) => {
+                return Err(ParsePersonError::ParseInt(p));
+            }
+        }
+
+        if name.is_empty() {
+            return Err(ParsePersonError::NoName);
+        }
+
+        Ok(Person {
+            name: name.to_owned(),
+            age,
+        })
     }
 }
 
