8b58f8d866802e1fc4ee956f9a34d0a5096c4892
OuOu2021 <1113117424@qq.com>
2024-09-30 14:59:41 +0800

    update

--- exercises/algorithm/algorithm3.rs
+++ exercises/algorithm/algorithm3.rs
@@ -1,12 +1,34 @@
 /*
-	sort
-	This problem requires you to implement a sorting algorithm
-	you can use bubble sorting, insertion sorting, heap sorting, etc.
+    sort
+    This problem requires you to implement a sorting algorithm
+    you can use bubble sorting, insertion sorting, heap sorting, etc.
 */
-// I AM NOT DONE
+use std::mem::swap;
 
-fn sort<T>(array: &mut [T]){
-	//TODO
+fn insertion_sort<T: PartialOrd>(array: &mut [T]) {
+    for i in 1..array.len() {
+        let mut j = i;
+        while j > 0 && array[j] < array[j - 1] {
+            array.swap(j, j - 1);
+            j -= 1;
+        }
+    }
+}
+
+fn bubble_sort<T: PartialOrd>(array: &mut [T]) {
+    let len = array.len();
+    for i in 1..len {
+        for j in 0..len - i {
+            if array[j] > array[j + 1] {
+                array.swap(j, j + 1);
+            }
+        }
+    }
+}
+
+fn sort<T: PartialOrd>(array: &mut [T]) {
+    // insertion_sort(array);
+    bubble_sort(array);
 }
 #[cfg(test)]
 mod tests {
@@ -18,13 +40,13 @@ mod tests {
         sort(&mut vec);
         assert_eq!(vec, vec![19, 37, 46, 57, 64, 73, 75, 91]);
     }
-	#[test]
+    #[test]
     fn test_sort_2() {
         let mut vec = vec![1];
         sort(&mut vec);
         assert_eq!(vec, vec![1]);
     }
-	#[test]
+    #[test]
     fn test_sort_3() {
         let mut vec = vec![99, 88, 77, 66, 55, 44, 33, 22, 11];
         sort(&mut vec);
